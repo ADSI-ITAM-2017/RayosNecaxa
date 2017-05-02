4339a48371e8ab600067f31b155ef59eec7be00d
Pablo Martínez <martipev2@hotmail.com>
2017-05-01 20:53:30 -0500

    Conexión de la landing page a la página de swiping de propiedades.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'landing_page/index'
 
-  resources :swipe_propiedades
+  get 'swipe_propiedades/index'
 
   root 'landing_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
