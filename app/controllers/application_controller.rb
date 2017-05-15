34735a4bcea6dc49fc10e454d1df24459e6c70d7
Pedro Lanzagorta <pelanmar1@gmail.com>
2017-05-15 10:34:06 -0500

    Agrego restriccion de uso de paginas segun tipo de usuario.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,30 @@ protect_from_forgery with: :null_session
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    helper_method :rol_usuario
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre,:apellido,:rol])
   end
+
+  def permiso_arrendador
+  	@rolUsuario = Usuario.where(id:current_usuario.id).first.rol
+  	if(@rolUsuario != 'Arrendador' || current_usuario.blank?)
+		    redirect_to('/')
+    end
+  end
+  def permiso_arrendatario
+    @rolUsuario = Usuario.where(id:current_usuario.id).first.rol
+    if(@rolUsuario != 'Arrendatario' || current_usuario.blank?)
+        redirect_to('/')
+    end
+  end
+  def rol_usuario 
+      if(!current_usuario.blank?)
+      return Usuario.where(id:current_usuario.id).first.rol
+    end
+  end
+
 end
--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,5 @@
 class GalleriesController < ApplicationController
-
+	before_filter :permiso_arrendatario
 	def index
 		@arrId = Arrendatario.where(usuario_id:current_usuario.id).first.id
 		@properties = Property.all()
